8628c97f9f96c6255e3b79e6704357ec5dc6d286
Ayush <ayush21052003@gmail.com>
2026-09-01 22:54:43 +0530

    Initialize PayFlow Project

---
+++ src/main/java/com/PayFlow/PayFlowApplication.java
@@ -0,0 +1,13 @@
+package com.PayFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayFlowApplication.class, args);
+	}
+
+}
